809ccde165ef5f9c4967618911f0f1c261925bcf
Jth1209 <xkgshand@naver.com>
2024-07-11 05:55:53 +0900

    Practice

---
+++ Java0710/src/Mac/Practice1.java
@@ -0,0 +1,15 @@
+package Mac;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		for(int j = 1; j<10;j++) {
+		     for(int i = 2; i <10; i++) {
+				   System.out.printf("%d X %d = %d\t",i,j,i*j);
+		   }
+		     System.out.println();
+		}
+
+	}
+
+}
